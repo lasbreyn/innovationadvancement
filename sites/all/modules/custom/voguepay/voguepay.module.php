29c51b56ccd7dc3efa5eb92653214c591034195b
Lasbrey Nwachuwku <lnwachukwu@unicon.net>
2016-01-06 17:09:46 -0600

    added vogue pay and Facebook login

---
+++ sites/all/modules/custom/voguepay/voguepay.module.php
@@ -0,0 +1,217 @@
+<?php
+  function voguepay_help($path, $arg) {
+      //$output = '<p>'.  t("voguepay is a module that extends functionality of sms framework.");
+      //    The line above outputs in ALL admin/module pages
+      $output = '';
+      switch ($path) {
+          case "admin/help/voguepay":
+          $output = '<p>'.  t("eCommerce - Voguepay - Drupal Plugin.") .'</p>';
+              break;
+      }
+      return $output;
+  } // function voguepay_help
+
+  /**
+   * Valid permissions for this module
+   * @return array An array of valid permissions for the voguepay module
+   */
+  function voguepay_perm() {
+      return array('administer voguepay');
+  }
+
+  /**
+   * Menu for this module
+   * @return array An array with this module's settings.
+   */
+  function voguepay_menu() {
+      $items = array();
+
+      //Link to the sms_zone admin page:
+      $items['admin/config/services/voguepay'] = array(
+        'title' => 'Voguepay',
+        'description' => 'Voguepay - Drupal Plugin',
+
+        'page callback'    => 'drupal_get_form',
+        'page arguments'   => array('voguepay_form'),
+
+        'access arguments' => array('administer nodes'),
+        'type' => MENU_NORMAL_ITEM,
+      );
+
+      $items['voguepay_notification'] = array(
+        'page callback' => 'gauth_response_handler',
+        'access callback' => TRUE,
+        'type' => MENU_CALLBACK,
+      );
+
+      return $items;
+  }
+
+  function voguepay_form() {
+     $form['merchant'] = array(
+        '#type' => 'textfield',
+        '#title' => t('VoguePay Merchant ID'),
+        '#default_value' => variable_get('vogue_merchant',''),
+        '#description' => t(''),
+        '#required' => TRUE
+  	  );
+
+  	$form['color'] = array(
+     '#type' => 'select',
+     '#title' => t('Button Color'),
+     '#default_value' => variable_get('vogue_color','blue'),
+     '#options' => array(
+          'blue' => t('Blue'),
+          'red' => t('Red'),
+          'green' => t('Green'),
+          'grey' => t('Grey'),
+        ),
+     '#description' => t(''),
+        '#required' => FALSE
+      );
+
+     $form['custom'] = array(
+        '#type' => 'textfield',
+        '#title' => t('Custom button'),
+        '#default_value' => variable_get('vogue_custom',''),
+        '#description' => t(''),
+        '#required' => FALSE
+  	  );
+
+      $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('Save Changes'),
+    );
+
+
+  	  return $form;
+  }
+
+  function voguepay_form_submit(&$form, $form_state) {
+
+    $merchant=$form_state['values']['merchant'];
+    $color=$form_state['values']['color'];
+    $custom=$form_state['values']['custom'];
+
+    variable_set('vogue_merchant',$merchant);
+    variable_set('vogue_color',$color);
+    variable_set('vogue_custom',$custom);
+
+    drupal_set_message(t("Your changes were saved successfully."));
+
+    $form_state['redirect'] = 'voguepay';
+  }
+
+  function voguepay_nodeapi(&$node, $op, $a3 = NULL, $a4 = NULL) {
+
+    if($op=="alter") {
+
+      $sitename  = variable_get('site_name', '');
+
+      // Get the body
+      $body = $node->body;
+
+      // Regular expression to fetch the voguepay tags
+      $regex = '/{voguepay\s*.*?}/i';
+      preg_match_all( $regex, $body, $matches );
+
+
+      // Fetch the default parameters
+      $merchant_id= variable_get('vogue_merchant','');
+      $alternate_button = variable_get('vogue_custom','');
+      $button_color = variable_get('vogue_color','blue');
+      $button = empty($alternate_button) ? 'http://voguepay.com/images/buttons/buynow_'.$button_color.'.png' : $alternate_button ;
+
+      foreach($matches[0] as $key => $match) {
+
+        $pattern = '/item\s*\((?<val>[^\(\)]+)\)/';
+        preg_match($pattern, $match, $m);
+        $item = $m['val'];
+        $pattern = '/price\s*\((?<val>[^\(\)]+)\)/';
+        preg_match($pattern, $match, $m);
+        $price = $m['val'];
+        $pattern = '/description\s*\((?<val>[^\(\)]+)\)/';
+        preg_match($pattern, $match, $m);
+        $description = empty($m['val']) ? $item.' at '.number_format($price,2) : $m['val'];
+
+        $f = '<form method="POST" action="https://voguepay.com/pay/">
+        <input type="hidden" name="v_merchant_id" value="'.$merchant_id.'" />
+        <input type="hidden" name="memo" value="'.$item.' ('.number_format($price,2).') order from '.$sitename.'" />
+        <input type="hidden" name="item_1" value="'.$item.'" />
+        <input type="hidden" name="description_1" value="'.$description.'" />
+
+        <input type="hidden" name="notify_url" value="'.$sitename.'/voguepay_notification" />
+        <input type="hidden" name="success_url" value="'.$sitename.'/voguepay_thank_you" />
+        <input type="hidden" name="fail_url" value="'.$sitename.'/voguepay_failed" />
+
+        <input type="hidden" name="price_1" value="'.$price.'" />
+        <input type="hidden" name="total" value="'.$price.'" />
+        <input type="hidden" name="notify_url" value='.$sitename.'/voguepay_notification" />
+        <input type="image" src="'.$button.'" alt="Pay with VoguePay" />
+        </form>';
+
+        $body = str_replace($match,$f,$body);
+      }
+
+      $node->body=$body;
+    }
+  }
+
+  function voguepay_node_view($node, $view_mode, $langcode) {
+    switch ($node->type) {
+      case 'conference':
+      drupal_set_message( '<pre>'.print_r($node->content, TRUE). '</pre>');
+/*
+        $node->content['extra_link'] = array(
+            '#weight' => 10,
+            '#theme' => 'link',
+            '#path' => 'path_to_content',
+            '#text' => t('An extra link'),
+            '#options' => array(
+                'attributes' => array(),
+                'html' => FALSE
+            ),
+        );
+*/
+        break;
+    }
+  }
+
+  function voguepay_getTransactionStatus() {
+    ##Check if transaction ID has been submitted
+    $merchant_id= variable_get('vogue_merchant','');
+
+    if( count(trim($merchant_id)) < 2) exit;
+
+    if(isset($_POST['transaction_id'])){
+    	//get the full transaction details as an json from voguepay
+    	$json = file_get_contents('https://voguepay.com/?v_transaction_id='.$_POST['transaction_id'].'&type=json');
+    	//create new array to store our transaction detail
+    	$transaction = json_decode($json, true);
+
+    	/*
+    	Now we have the following keys in our $transaction array
+    	$transaction['merchant_id'],
+    	$transaction['transaction_id'],
+    	$transaction['email'],
+    	$transaction['total'],
+    	$transaction['merchant_ref'],
+    	$transaction['memo'],
+    	$transaction['status'],
+    	$transaction['date'],
+    	$transaction['referrer'],
+    	$transaction['method']
+    	*/
+
+    	if($transaction['total'] == 0)die('Invalid total');
+    	if($transaction['status'] != 'Approved')die('Failed transaction');
+    	if($transaction['merchant_id'] != $merchant_id)die('Invalid merchant');
+
+    	/*You can do anything you want now with the transaction details or the merchant reference.
+    	You should query your database with the merchant reference and fetch the records you saved for this transaction.
+    	Then you should compare the $transaction['total'] with the total from your database.*/
+    }
+  }
+
+
+
